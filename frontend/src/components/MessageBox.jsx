adc465fd1a6469b896ef317a192bffa77547cafc
Juan <juan_ancap@protonmail.com>
2023-07-11 09:11:54 -0300

    Agregado de componentes LoadingBox y MessageBox

---
+++ frontend/src/components/MessageBox.jsx
@@ -0,0 +1,11 @@
+import Alert from 'react-bootstrap/Alert';
+import PropTypes from 'prop-types';
+
+export const MessageBox = (props) => {
+  return <Alert variant={props.variant || 'info'}>{props.children}</Alert>;
+};
+
+MessageBox.propTypes = {
+  variant: PropTypes.string,
+  children: PropTypes.string,
+};
