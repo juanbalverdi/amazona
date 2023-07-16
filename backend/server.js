6a7eef723a80ba39965a890f65613e8ce6f639dd
Juan <juan_ancap@protonmail.com>
2023-07-16 02:38:49 -0300

    Create sample users, install bcrypt

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
+app.use('/api/seed', seedRouter);
+
+app.use('/api/products', productRouter);
 
 const port = process.env.PORT || 5000;
 
